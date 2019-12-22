4ce8c85d0757190b98f782ec964bc8456efaf97f
zz laptop <flyingman_pan@outlook.com>
2019-12-22 15:39:23 +0800

    fix firmware

--- playground/localController.py
+++ playground/localController.py
@@ -1,11 +1,14 @@
 from inputs import get_gamepad
 import serial
-ser=serial.Serial('COM3',115200)
-while 1:
+ser=serial.Serial('COM4',115200)
+prevB=0
+while True:
     events = get_gamepad()
-
     #X,Y [-32768,+32768]
     #Z,RZ(RT,LT) [0,255]
+    local=ser.read_all()
+    if len(local)>0:
+        print(local.decode())
     for event in events:
         # print(event.ev_type, event.code, event.state)
         if event.code=='ABS_X':
@@ -20,3 +23,10 @@ while 1:
         if event.code=='ABS_Z':
             snd='L'+str(int(event.state))+'\r'
             ser.write(snd.encode())
+        if event.code=='BTN_EAST':
+            if event.state==0 and prevB==1:
+                ser.close()
+                ser.open()
+                exit(0)
+            else:
+                prevB=event.state
